7aa743e0d4c488d3425ae71ad1095f8493a0e469
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-09-15 20:50:53 +0600

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <foter>
+      <p>Lorem Ipsum colaps &copy; website Name {new Date().getFullYear()}</p>
+    </foter>
+  );
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Webpage!</h1>
+      <nav>
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">SERVICES</a></li>
+            <li><a href="#">CONTACT</a></li>
+        </ul>
+      </nav>
+      <hr></hr>
+    </header>
+  );
+}
+
+export default Header
